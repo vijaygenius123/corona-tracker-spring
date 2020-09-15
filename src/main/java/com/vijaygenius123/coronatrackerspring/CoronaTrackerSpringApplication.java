c3e88a9888a3a2b266445b0f5a4c58b1cc2fc4cf
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-15 22:10:15 +0530

    Parsing The Data And Enabled Scheduler

--- src/main/java/com/vijaygenius123/coronatrackerspring/CoronaTrackerSpringApplication.java
+++ src/main/java/com/vijaygenius123/coronatrackerspring/CoronaTrackerSpringApplication.java
@@ -2,8 +2,10 @@ package com.vijaygenius123.coronatrackerspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronaTrackerSpringApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/vijaygenius123/coronatrackerspring/services/CoronaVirusDataService.java
+++ src/main/java/com/vijaygenius123/coronatrackerspring/services/CoronaVirusDataService.java
@@ -1,9 +1,13 @@
 package com.vijaygenius123.coronatrackerspring.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -15,6 +19,7 @@ public class CoronaVirusDataService {
     public static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
@@ -26,5 +31,14 @@ public class CoronaVirusDataService {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
+
+        StringReader csvBodyReader = new StringReader(response.body());
+        Iterable<CSVRecord>  records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+
+        for(CSVRecord record: records){
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
+
     }
 }
