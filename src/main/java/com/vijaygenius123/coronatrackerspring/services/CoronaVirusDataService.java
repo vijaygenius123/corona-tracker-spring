cbb39ebfed702e86e7cf0941a985d8c199695f9f
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-15 22:31:02 +0530

    Created LocationStats Model

--- src/main/java/com/vijaygenius123/coronatrackerspring/services/CoronaVirusDataService.java
+++ src/main/java/com/vijaygenius123/coronatrackerspring/services/CoronaVirusDataService.java
@@ -1,5 +1,6 @@
 package com.vijaygenius123.coronatrackerspring.services;
 
+import com.vijaygenius123.coronatrackerspring.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,16 +13,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
 
     public static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats = new ArrayList<>();
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
 
+        List<LocationStats> newStats = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
@@ -36,9 +41,15 @@ public class CoronaVirusDataService {
         Iterable<CSVRecord>  records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
 
         for(CSVRecord record: records){
-            String state = record.get("Province/State");
-            System.out.println(state);
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1 )));
+            System.out.println(locationStat.toString());
+            newStats.add(locationStat);
         }
+
+        this.allStats = newStats;
 
     }
 }
