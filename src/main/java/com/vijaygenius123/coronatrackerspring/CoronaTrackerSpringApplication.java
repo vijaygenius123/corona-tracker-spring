1edf198b37c7f53ce131c21be7338fd62d6da547
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-15 20:08:07 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/coronatrackerspring/CoronaTrackerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.coronatrackerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaTrackerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaTrackerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/coronatrackerspring/CoronaTrackerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.coronatrackerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTrackerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
